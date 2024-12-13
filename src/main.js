d9110e6f623b6170d4771f61494cf32a3b7d40cb
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-12-13 17:52:17 +0200

    fixed functions, added button

--- src/main.js
+++ src/main.js
@@ -2,36 +2,21 @@ import { Application, Assets, Graphics, Sprite } from "pixi.js";
 import "./style.css";
 
 const canvas = document.getElementById("canvas");
+const startButton = document.getElementById("start-button");
 
-(async () => {
-  const app = new Application();
+const app = new Application();
 
-  await app.init({
-    view: canvas,
-    width: canvas.width,
-    height: canvas.height,
-    resizeTo: window,
-  });
-  // document.body.appendChild(app.canvas);
-
-  //  додаємо background
-  const bgTexture = await Assets.load("/src/img/starry-sky.png");
+//  додаємо background
+async function addBackground(imageBg) {
+  const bgTexture = await Assets.load(imageBg);
   const background = new Sprite(bgTexture);
   background.width = app.screen.width;
   background.height = app.screen.height;
   app.stage.addChild(background);
+}
 
-  // додаємо космічний корабель
-  const spaceshipTexture = await Assets.load("./src/img/spaceship.png");
-  const spaceship = new Sprite(spaceshipTexture);
-  spaceship.anchor.set(0.5);
-  spaceship.width = 100;
-  spaceship.height = 120;
-  spaceship.x = app.screen.width / 2;
-  spaceship.y = app.screen.height - spaceship.height / 2;
-  app.stage.addChild(spaceship);
-
-  // додаємо зірки на background
+// додаємо зірки на background
+async function addStars() {
   const starCount = 100;
   const starsGraphics = new Graphics();
 
@@ -47,49 +32,24 @@ const canvas = document.getElementById("canvas");
   }
 
   app.stage.addChild(starsGraphics);
+}
 
-  // додаємо астероїди
+// додаємо космічний корабель
+async function addSpaceShip(imageSpaceShipe) {
+  const spaceshipTexture = await Assets.load(imageSpaceShipe);
+  const spaceship = new Sprite(spaceshipTexture);
+  spaceship.anchor.set(0.5);
+  spaceship.width = 100;
+  spaceship.height = 120;
+  spaceship.x = app.screen.width / 2;
+  spaceship.y = app.screen.height - spaceship.height / 2;
+  app.stage.addChild(spaceship);
 
-  // const asteroidsContainer = new Container();
-  // asteroidsContainer.x = 25;
-  // asteroidsContainer.y = 70;
-  // const asteroidsContainerWidth = app.screen.width - 50;
-  // const asteroidsContainerHeight = app.screen.height - 200;
+  await setupSpaceShip(spaceship, app);
+}
 
-  // const containerBorder = new Graphics()
-  //   .rect(0, 0, asteroidsContainerWidth, asteroidsContainerHeight)
-  //   .fill("white")
-  //   .stroke({ color: 0xff0000, pixelLine: true });
-
-  // asteroidsContainer.addChild(containerBorder);
-  // app.stage.addChild(asteroidsContainer);
-  // asteroidsContainer.addChild(asteroid);
-  // console.log(asteroidsContainer);
-
-  const asteroidTexture = await Assets.load("/src/img/asteroid.png");
-
-  function createAsteroid() {
-    const asteroid = new Sprite(asteroidTexture);
-    asteroid.anchor.set(0.5);
-    asteroid.width = 50;
-    asteroid.height = 60;
-
-    asteroid.x = Math.random() * app.screen.width - 100;
-    asteroid.y = Math.random() * (app.screen.height - 200);
-
-    return asteroid;
-  }
-
-  function addRandomAsteroids(count) {
-    for (let i = 0; i < count; i++) {
-      const asteroid = createAsteroid();
-      app.stage.addChild(asteroid);
-    }
-  }
-  addRandomAsteroids(5);
-
-  // додаємо управління рухом корабля
-
+// додаємо управління рухом корабля
+async function setupSpaceShip(spaceship, app) {
   let moveLeft = false;
   let moveRight = false;
   const shipSpeed = 8;
@@ -120,4 +80,74 @@ const canvas = document.getElementById("canvas");
       spaceship.x += shipSpeed;
     }
   });
+}
+
+// додаємо астероїди
+async function addAsteroids(imageAsteroid) {
+  const asteroidTexture = await Assets.load(imageAsteroid);
+
+  function createAsteroid() {
+    const asteroid = new Sprite(asteroidTexture);
+    asteroid.anchor.set(0.5);
+    asteroid.width = 50;
+    asteroid.height = 60;
+
+    asteroid.x = Math.random() * (app.screen.width - 100) + 50;
+    asteroid.y = Math.random() * (app.screen.height - 200) + 50;
+
+    return asteroid;
+  }
+
+  function addRandomAsteroids(count) {
+    for (let i = 0; i < count; i++) {
+      const asteroid = createAsteroid();
+      app.stage.addChild(asteroid);
+    }
+  }
+  addRandomAsteroids(5);
+}
+
+// ініціалізуємо початок гри
+async function onClickStartButton() {
+  startButton.onclick = () => onStartGame();
+
+  return startButton;
+}
+
+async function onStartGame(params) {
+  console.log("game started");
+
+  await addAsteroids("/src/img/asteroid.png");
+}
+
+// ==================================================================
+
+(async () => {
+  await app.init({
+    canvas: canvas,
+    width: canvas.width,
+    height: canvas.height,
+    // resizeTo: window,
+  });
+
+  await addBackground("/src/img/starry-sky.png");
+  await addStars();
+  await addSpaceShip("./src/img/spaceship.png");
+  await onClickStartButton();
 })();
+
+// const asteroidsContainer = new Container();
+// asteroidsContainer.x = 25;
+// asteroidsContainer.y = 70;
+// const asteroidsContainerWidth = app.screen.width - 50;
+// const asteroidsContainerHeight = app.screen.height - 200;
+
+// const containerBorder = new Graphics()
+//   .rect(0, 0, asteroidsContainerWidth, asteroidsContainerHeight)
+//   .fill("white")
+//   .stroke({ color: 0xff0000, pixelLine: true });
+
+// asteroidsContainer.addChild(containerBorder);
+// app.stage.addChild(asteroidsContainer);
+// asteroidsContainer.addChild(asteroid);
+// console.log(asteroidsContainer);
